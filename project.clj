adca10710a611e0b50ee2b8b33149a3e4549f9d5
Ian Henry <ianthehenry@gmail.com>
2014-08-09 13:41:32 -0400

    upgraded async; added a prod build type

--- project.clj
+++ project.clj
@@ -2,13 +2,19 @@
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [org.clojure/clojurescript "0.0-2280"]
                  [om "0.7.0"]
-                 [org.clojure/core.async "0.1.303.0-886421-alpha"]]
+                 [org.clojure/core.async "0.1.319.0-6b1aca-alpha"]
+                 ]
   :plugins [[lein-cljsbuild "1.0.3"]]
   :cljsbuild {:builds [{:id "dev"
                         :source-paths ["src"]
-                        :compiler {:output-to "out/main.js"
-                                   :output-dir "out"
+                        :compiler {:output-to "out/dev/main.js"
+                                   :output-dir "out/dev"
                                    :optimizations :none
-                                   :source-map true}}]})
-
-
+                                   :source-map "out/dev/main.js.map"}}
+                       {:id "prod"
+                        :source-paths ["src"]
+                        :compiler {:output-to "out/prod/main.js"
+                                   :output-dir "out/prod"
+                                   :optimizations :advanced
+                                   :pretty-print false
+                                   :source-map "out/prod/main.js.map"}}]})
