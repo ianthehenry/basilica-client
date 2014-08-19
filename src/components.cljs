a7d37077a74c39525d64bfacf68276308f8149cf
Ian Henry <ianthehenry@gmail.com>
2014-08-19 00:35:21 -0400

    added a tooltip for socket state

--- src/components.cljs
+++ src/components.cljs
@@ -139,5 +139,8 @@
    (dom/div #js {:id "header"}
             (dom/h1 nil (dom/a #js {:href conf/site-base} "Basilica"))
             (dom/div (with-classes {:id "socket-status"
+                                    :onTouchStart (fn [e]) ; allows the hover state to work on mobile safari
                                     :title (status-titles socket-state)}
-                       (name socket-state))))))
+                       (name socket-state))
+                     (dom/div (classes "tooltip")
+                              (status-titles socket-state))))))
