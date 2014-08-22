3a826014da7c5b669f11803bd23bbc0a0869e9a2
Ian Henry <ianthehenry@gmail.com>
2014-08-21 23:34:34 -0400

    bump to om 0.7.1

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
 (defproject basilica-client "0.1.0-SNAPSHOT"
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [org.clojure/clojurescript "0.0-2280"]
-                 [om "0.7.0"]
+                 [om "0.7.1"]
                  [org.clojure/core.async "0.1.319.0-6b1aca-alpha"]
                  [secretary "1.2.0"]
                  ]
