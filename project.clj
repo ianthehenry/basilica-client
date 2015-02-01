76c0c71e80ee57debb585bbc7a492912c365baa9
Ian Henry <ianthehenry@gmail.com>
2015-02-01 15:02:34 -0500

    update to om 0.8.7
    prod build might be broken. who knows?

--- project.clj
+++ project.clj
@@ -1,15 +1,17 @@
 (defproject basilica-client "0.1.0-SNAPSHOT"
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [org.clojure/clojurescript "0.0-2740"]
-                 [om "0.7.1"]
+                 [org.omcljs/om "0.8.7"]
                  [org.clojure/core.async "0.1.346.0-17112a-alpha"]
                  [secretary "1.2.0"]
                  ]
   :plugins [[lein-cljsbuild "1.0.4"]]
   :cljsbuild {:builds [{:id "dev"
                         :source-paths ["src" "conf/dev"]
-                        :compiler {:output-to "out/dev/main.js"
+                        :compiler {:main basilica.core
+                                   :output-to "out/dev/main.js"
                                    :output-dir "out/dev"
+                                   :asset-path "http://localhost:3333"
                                    :optimizations :none
                                    :source-map "out/dev/main.js.map"}}
                        {:id "prod"
@@ -19,7 +21,7 @@
                                    :optimizations :advanced
                                    :pretty-print false
                                    :output-wrapper false
-                                   :preamble ["react/react.min.js" "momentjs/min/moment.min.js" "marked/lib/marked.js"]
-                                   :externs ["react/react.js" "momentjs/moment.js" "marked/lib/marked.js"]
+                                   :preamble ["momentjs/min/moment.min.js" "marked/lib/marked.js"]
+                                   :externs ["momentjs/moment.js" "marked/lib/marked.js"]
                                    :closure-warnings {:externs-validation :off
                                                       :non-standard-jsdoc :off}}}]})
