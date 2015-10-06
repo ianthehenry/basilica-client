635896b3777e4e3f8284d946eced061f81050158
Ian Henry <ianthehenry@gmail.com>
2015-10-05 22:31:29 -0400

    bump clojurescript
    And other dependencies.

--- project.clj
+++ project.clj
@@ -1,9 +1,9 @@
 (defproject basilica-client "0.1.0-SNAPSHOT"
-  :dependencies [[org.clojure/clojure "1.6.0"]
-                 [org.clojure/clojurescript "0.0-2740"]
+  :dependencies [[org.clojure/clojure "1.7.0"]
+                 [org.clojure/clojurescript "1.7.48"]
                  [org.omcljs/om "0.8.7"]
                  [org.clojure/core.async "0.1.346.0-17112a-alpha"]
-                 [secretary "1.2.0"]
+                 [secretary "1.2.3"]
                  ]
   :plugins [[lein-cljsbuild "1.0.4"]]
   :cljsbuild {:builds [{:id "dev"
